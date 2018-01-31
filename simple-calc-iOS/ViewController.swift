eccaa0d6da76af4f21f42b7aee0554cbef81e589
Yicheng Jiang <orange@D-173-250-201-60.dhcp4.washington.edu>
2018-01-30 23:55:38 -0800

    finish the calculator

--- simple-calc-iOS/ViewController.swift
+++ simple-calc-iOS/ViewController.swift
@@ -9,7 +9,87 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var input: String = ""
+    var firstNum: Double = 0.0
+    var inputNums: [Double] = []
+    var op: String = ""
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func Numbers(_ sender: UIButton) {
+        input += (sender.titleLabel?.text!)!
+        if (op != ""){
+            displayLabel.text = String(firstNum) + op + String(input)
+        }else {
+            displayLabel.text = String(input)
+        }
+    }
+    
 
+    @IBAction func Result(_ sender: UIButton) {
+        let result = calculate()
+        displayLabel.text = String(result)
+    }
+    
+    @IBAction func Clear(_ sender: UIButton) {
+        input = ""
+        firstNum = 0
+        inputNums = []
+        op = ""
+        displayLabel.text = "0"
+    }
+    
+    
+    @IBAction func Operator(_ sender: UIButton) {
+        op = (sender.titleLabel?.text!)!
+        firstNum = Double(input)!
+        displayLabel.text = String(firstNum) + op
+        if op == "count" || op == "avg" {
+            self.inputNums.append(firstNum)
+        }
+        if op == "fact" {
+            let fact = getFactorial(Int(Double(input)!))
+            displayLabel.text = String(fact)
+        }
+        input = ""
+    }
+    
+    private func calculate() -> Double{
+        switch op {
+        case "+":
+            return firstNum + Double(input)!
+        case "-":
+            return firstNum - Double(input)!
+        case "/":
+            return firstNum / Double(input)!
+        case "%":
+            return Double(Int(firstNum) % Int(Double(input)!))
+        case "*":
+            return firstNum * Double(input)!
+        case "count":
+            self.inputNums.append(Double(input)!)
+            return Double(self.inputNums.count)
+        case "avg":
+            self.inputNums.append(Double(input)!)
+            var sum = 0.0
+            for index in 0...inputNums.count - 1 {
+                sum += inputNums[index]
+            }
+            return sum / Double((inputNums.count))
+        default:
+            return 0
+        }
+    }
+    
+    // returns factorial of a given number
+    private func getFactorial (_ num: Int) -> Int{
+        var res = 1
+        for i in 1..<num + 1 {
+            res *= i
+        }
+        return res
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
